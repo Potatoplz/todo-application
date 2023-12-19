b89a4ff401cf0f19a5d64a05f46e63f3f90ec950
Potatoplz <beneh0710@gmail.com>
2023-12-19 14:18:49 +0900

    버튼 클릭시 할일 목록 업데이트

--- todo-frontend/src/AddTodo.js
+++ todo-frontend/src/AddTodo.js
@@ -5,11 +5,45 @@ import { Button, Grid, TextField } from "@mui/material";
 const AddTodo = (props) => {
   // 사용자의 입력을 저장할 오브젝트
   const [item, setItem] = useState({ title: "" });
+  const addItem = props.addItem;
+  //console.log("addItem >>>", addItem);
+
+  /**
+   * 입력 필드의 변화를 처리하는 이벤트 핸들러 역할을 하는 함수
+   * 사용자가 인풋필드에 키를 하나 입력할 때마다 실행되며, 인풋필드에 담긴 문자열을 js object에 저장
+   * TextField에 onChange라는 이벤트에 연결해 놓았으므로, TextField에 변화가 생기면 이 함수가 실행됨.
+   * e는 onChange이벤트가 발동하면 생성되는 이벤트 객체로 e.target.value를 통해 실제 입력값에 접근할 수 있다.
+   * @param {Event} e - 이벤트 핸들러 함수에 전달되는 이벤트 객체(JS내장기능). 이 객체는 이벤트와 관련된 다양한 정보와 메소드를 포함하고 있다. 예를 들어, 사용자가 입력 필드에 입력하거나 버튼을 클릭할 때 발생하는 이벤트에 대한 상세 정보를 담고 있다. `e.target` 속성은 이벤트가 발생한 DOM 요소를 참조하고, `e.target.value`는 해당 요소의 현재 값을 나타낸다.
+   */
+  const onInputChange = (e) => {
+    console.log("onInputChange");
+    setItem({ title: e.target.value }); // 텍스트입력값이 담겨있는 e.target.value를 item에 세팅.
+  };
+
+  /**
+   * 버튼 클릭 이벤트
+   * + 버튼을 클릭하면 addItem 함수에 item이라는 매개변수를 전달하고(App.js에 있는 함수로 전달), item을 초기화해서 TextField입력값을 비워준다.
+   */
+  const onButtonClick = () => {
+    console.log("AddTodo.js onButtonClick");
+    addItem(item); // props로 받아온 addItem 함수
+    setItem({ title: "" });
+  };
 
   return (
     <Grid container style={{ marginTop: 20 }}>
       <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-        <TextField placeholder="Add Todo here" fullWidth />
+        <TextField
+          placeholder="Add Todo here"
+          fullWidth
+          onChange={onInputChange}
+          value={item.title}
+          /**
+           * value={item.title}
+           * 사용자가 필드에 텍스트를 입력하면 onInputChange 이벤트 핸들러가 호출되어,
+           * item 상태를 업데이트하고, 이 변경된 item.title 값이 다시 TextField에 반영
+           */
+        />
       </Grid>
       <Grid xs={1} md={1} item>
         <Button
@@ -17,6 +51,7 @@ const AddTodo = (props) => {
           style={{ height: "100%" }}
           color="secondary"
           variant="outlined"
+          onClick={onButtonClick}
         >
           +
         </Button>
--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -5,8 +5,10 @@ import "./App.css";
 import Todo from "./Todo";
 import AddTodo from "./AddTodo";
 
+// App 컴포넌트 정의
 function App() {
-  const [items, setItem] = useState([
+  // 할 일 목록을 상태로 관리하기 위한 useState 훅
+  const [items, setItems] = useState([
     {
       id: "0",
       title: "Hello World 1",
@@ -19,21 +21,38 @@ function App() {
     },
   ]);
 
+  /**
+   * addItem 함수의 매개변수(item)는 AddTodo.js에서 받음.
+   * 새로운 할 일(item)을 기존 할 일 목록에 추가하는 함수
+   * @param {Object} item - item이라는 객체를 매개변수로 받는다. item은 AddTodo.js에서 +버튼 클릭시 전달된다.
+   */
+  const addItem = (item) => {
+    item.id = "ID-" + items.length; // 새 항목에 고유 ID 생성
+    item.done = false; // 새 항목의 완료 상태 초기화
+
+    // items 상태를 업데이트하기 위해 setItems 사용
+    console.log("items 업데이트 전 : ", items);
+    setItems([...items, item]); // 기존 items에 새 item 추가
+    console.log("items 업데이트 후 : ", items);
+  };
+
+  // 할 일 목록을 표시하는 부분
   let todoItems = items.length > 0 && (
     <Paper style={{ margin: 16 }}>
       <List>
         {items.map((item) => (
-          <Todo item={item} key={item.id} />
+          <Todo item={item} key={item.id} /> // 각 할 일을 Todo 컴포넌트로 렌더링
         ))}
       </List>
     </Paper>
   );
 
+  // 렌더링 부분
   return (
     <div className="App">
       <Container maxWidth="md">
-        <AddTodo />
-        <div className="TodoList">{todoItems}</div>
+        <AddTodo addItem={addItem} /> {/*AddTodo 컴포넌트에 addItem 함수 전달*/}
+        <div className="TodoList">{todoItems}</div> {/*할 일 목록 렌더링*/}
       </Container>
     </div>
   );
